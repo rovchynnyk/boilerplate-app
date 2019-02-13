5a3ebf8bec3db534185c35e6b24b63fedda0d7ed
ov__roman <roman.ovchynnyk@just-eat.com>
2019-02-13 23:20:31 +0200

    updated packages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import './styles.css';
-import { hot } from 'react-hot-loader';
-import styled from 'styled-components'
+import React from "react";
+import "./styles.css";
+import { hot } from "react-hot-loader";
+import styled from "styled-components";
 
 const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
+	font-size: 1.5em;
+	text-align: center;
+	color: palevioletred;
 `;
 
 const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
+	padding: 4em;
+	background: papayawhip;
 `;
 
-
 class App extends React.Component {
-    render() {
-        return (
-            <Wrapper>
-                <Title>Hello world!</Title>
-            </Wrapper>
-        );
-    }
+	render() {
+		return (
+			<Wrapper>
+				<Title>Hello world!</Title>
+			</Wrapper>
+		);
+	}
 }
 
 export default hot(module)(App);
