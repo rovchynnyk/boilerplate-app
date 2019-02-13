e7f3671ca80fc04bd4a4460367f7a176269221eb
ov__roman <roman.ovchynnyk@just-eat.com>
2019-02-13 23:22:28 +0200

    updated jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    setupTestFrameworkScriptFile: '<rootDir>/testSetup.js',
-    moduleNameMapper: {
-        '\\.(css|less)$': 'identity-obj-proxy'
-    }
+	setupFilesAfterEnv: ["<rootDir>/testSetup.js"],
+	moduleNameMapper: {
+		"\\.(css|less)$": "identity-obj-proxy"
+	}
 };
